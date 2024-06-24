25b159fdaa5c8948f8ccced4d40bc4c4565d0a46
muhsinachipra <muhsinachipra@gmail.com>
2024-06-24 18:02:05 +0530

    dynamicaly populate category on expert register

--- src/pages/expert/Register.tsx
+++ src/pages/expert/Register.tsx
@@ -14,6 +14,7 @@ import { storage } from "../../app/firebase/config";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { v4 as uuidv4 } from "uuid"
 import { Link } from "react-router-dom";
+import { useGetCategoryDataQuery } from "../../slices/api/adminApiSlice";
 
 export default function Register() {
 
@@ -21,6 +22,9 @@ export default function Register() {
     const dispatch = useDispatch()
     const [isSumbit, setSubmit] = useState(false)
     const navigate = useNavigate()
+    const { data: categoryData } = useGetCategoryDataQuery({ page: 1, limit: 100 });
+    const categories = categoryData?.data || [];
+
 
     const [profilePicture, setProfilePic] = useState<File | null>(null);
     const [resume, setResume] = useState<File | null>(null);
@@ -155,12 +159,18 @@ export default function Register() {
                                 <div>
                                     <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Field of Expertise</label>
                                     <select name="category" value={values.category} onChange={handleChange} id="category" className="bg-neutral-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required={true}>
-                                        <option value="">Select Field of Expertise</option>
+                                        {/* <option value="">Select Field of Expertise</option>
                                         <option value="Doctor">Doctor</option>
                                         <option value="Lawyer">Lawyer</option>
                                         <option value="Career Advisor">Career Advisor</option>
                                         <option value="Financial Advisor">Financial Advisor</option>
-                                        <option value="psychiatrist">psychiatrist</option>
+                                        <option value="psychiatrist">psychiatrist</option> */}
+                                        <option value="">Select Field of Expertise</option>
+                                        {categories.map((category) => (
+                                            <option key={category._id} value={category.categoryName}>
+                                                {category.categoryName}
+                                            </option>
+                                        ))}
                                     </select>
                                     {errors.category && touched.category && (
                                         <div className="text-red-500">{errors.category}</div>
