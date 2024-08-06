3f5dc773481a36b7901ec7adb6410964e1cdffde
muhsinachipra <muhsinachipra@gmail.com>
2024-08-06 23:33:28 +0530

    "Move firebase config to env variables"

--- src/app/firebase/config.ts
+++ src/app/firebase/config.ts
@@ -3,14 +3,6 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDIjA4dfSk1uqRr-1F4lVqDTN1Il7A9nYE",
-//     authDomain: "askexpert-1716207437810.firebaseapp.com",
-//     projectId: "askexpert-1716207437810",
-//     storageBucket: "askexpert-1716207437810.appspot.com",
-//     messagingSenderId: "614409014364",
-//     appId: "1:614409014364:web:1133f18b14b5fea1cd2a99"
-// };
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
