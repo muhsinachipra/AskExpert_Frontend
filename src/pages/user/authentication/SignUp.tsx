0576192f7f1932ac3b691d54cb6c0a092fd7e433
muhsinachipra <muhsinachipra@gmail.com>
2024-08-10 14:16:41 +0530

    Update SignUp page layout for responsiveness

--- src/pages/user/authentication/SignUp.tsx
+++ src/pages/user/authentication/SignUp.tsx
@@ -89,8 +89,8 @@ export default function SignUp() {
 
 
     return (
-        <section className="bg-neutral-200 dark:bg-gray-900">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <section className="bg-neutral-200 dark:bg-gray-900 min-h-screen">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto min-h-screen lg:py-0">
                 <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                     <img className="w-8 h-8 mr-2" src="/Ask.svg" alt="logo" />
                     AskExpert
