0a9ec1871b8c309a98bfb3f9afd9b2495fd0e5b8
muhsinachipra <muhsinachipra@gmail.com>
2024-08-10 14:24:52 +0530

    Update section className to include min-h-screen

--- src/components/ResetPassword.tsx
+++ src/components/ResetPassword.tsx
@@ -11,7 +11,7 @@ interface ResetPasswordProps {
 
 const ResetPassword: React.FC<ResetPasswordProps> = ({ userId, handleResetPassword }) => {
     return (
-        <section className="bg-neutral-200 dark:bg-gray-900">
+        <section className="bg-neutral-200 dark:bg-gray-900  min-h-screen">
             {userId ? (
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
