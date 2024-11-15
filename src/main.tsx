a8185d61ad9b77c3288b2140977022abe33da96f
Muhsin Achipra <muhsinachipra@gmail.com>
2024-11-15 21:29:56 +0530

    last

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
+// hello
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
     <Provider store={store}>
